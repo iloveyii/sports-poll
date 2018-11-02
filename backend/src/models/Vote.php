5ba97db9f206b556049f2fcaac5a9f3e03c2bd8c
Hazrat Ali <ali@triggybet.com>
2018-11-02 18:14:16 +0100

    Added model Vote

---
+++ backend/src/models/Vote.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Models;
+
+
+class Vote extends Model
+{
+    /**
+     * @var null|int
+     */
+    public $id;
+    public $event_id;
+    public $user_id;
+    public $winner_id;
+
+    /**
+     * @var string
+     */
+    private $tableName = 'vote';
+
+
+    /**
+     * Post constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Pass request object to this method to set the object attributes
+     * @param array $attributes
+     */
+    public function setAttributes($attributes)
+    {
+        $this->id = isset($attributes['id'])  ? $attributes['id'] : null;
+        $this->event_id = $attributes['event_id'];
+        $this->user_id = $attributes['user_id'];
+        $this->winner_id = $attributes['winner_id'];
+        $this->isNewRecord = $this->id === null ? true : false;
+    }
+
+    /**
+     * These are the validation rules for the attributes
+     * @return array
+     */
+    public function rules() : array
+    {
+        return [
+            'id' => ['integer'],
+            'title' => ['string', 'minLength'=>5, 'maxLength'=>140, 'alpha'],
+            'description' => ['string', 'minLength'=>15, 'maxLength'=>800, 'stripTags'],
+        ];
+    }
+
+    // CRUD
+
+    /**
+     * Creates a db post record
+     * @return bool
+     * @throws \Exception
+     */
+    public function create() : bool
+    {
+        $query = sprintf("INSERT INTO %s (event_id, user_id, winner_id) 
+                                 VALUES (:event_id, :user_id, :winner_id)", $this->tableName);
+        $params = [':event_id'=>$this->event_id, ':user_id'=>$this->user_id, ':winner_id'=>$this->winner_id];
+        return Database::connect()->insert($query, $params);
+    }
+
+    /**
+     * Reads all posts from db into associative array
+     * @return array
+     * @throws \Exception
+     */
+    public function readAll() : array
+    {
+        $query = sprintf("SELECT * FROM %s", $this->tableName);
+        $rows = Database::connect()->selectAll($query, []);
+
+        return $rows;
+    }
+
+    public function readColumnAll($columnName) : array
+    {
+        $query = sprintf("SELECT %s FROM %s", $columnName, $this->tableName);
+        $rows = Database::connect()->selectAll($query, []);
+        return $rows;
+    }
+
+    /**
+     * Updates the given record in DB using id in Request object
+     * @return bool
+     * @throws \Exception
+     */
+    public function update() : bool
+    {
+        $query = sprintf("UPDATE %s SET title=:title, description=:description WHERE id=:id", $this->tableName);
+        $params = [':id'=>$this->id, ':title'=>$this->title, ':description'=>$this->description];
+        $result = Database::connect()->update($query, $params);
+        return $result;
+    }
+
+    /**
+     * Deletes the post with id in Request object
+     * @return bool
+     * @throws \Exception
+     */
+    public function delete() : bool
+    {
+        $query = sprintf("DELETE FROM %s WHERE id=:id", $this->tableName);
+        $params = [':id'=>$this->id];
+        $result = Database::connect()->delete($query, $params);
+        return $result;
+    }
+
+}
