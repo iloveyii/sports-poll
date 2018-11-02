35ef4ea02e149da3e86c6debe4995129b16b53d1
Hazrat Ali <ali@triggybet.com>
2018-11-02 18:04:30 +0100

    Add necessary tables

--- backend/src/init.php
+++ backend/src/init.php
@@ -4,6 +4,9 @@ require_once 'config/app.php';
 
 use App\Models\Database;
 
+/**
+ * Create table event and import all data from json file
+ */
 $tableName = 'event';
 $dropTable = "DROP TABLE IF EXISTS {$tableName}";
 Database::connect()->exec($dropTable);
@@ -28,7 +31,9 @@ $event = new \App\Models\Event();
 $event->loadJsonFileToTable();
 print ("Imported json file to table $tableName. " . PHP_EOL);
 
-
+/**
+ * Create table winner and insert some data
+ */
 $tableName = 'winner';
 $dropTable = "DROP TABLE IF EXISTS {$tableName}";
 Database::connect()->exec($dropTable);
@@ -43,4 +48,38 @@ $insertData = "INSERT INTO $tableName (name) VALUES('home'),('draw'),('away')";
 Database::connect()->exec($insertData);
 print ("Inserted data into table $tableName. " . PHP_EOL);
 
+/**
+ * Create table user and insert default user admin with pass admin
+ */
+$tableName = 'user';
+$dropTable = "DROP TABLE IF EXISTS {$tableName}";
+Database::connect()->exec($dropTable);
 
+$createTable = "CREATE TABLE $tableName(
+  id INT( 11 ) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  username CHAR( 10 ), 
+  password CHAR( 60 )
+  );";
+Database::connect()->exec($createTable);
+print ("Created table $tableName. " . PHP_EOL);
+$hash = password_hash('admin', PASSWORD_BCRYPT);
+$insertData = "INSERT INTO $tableName (username, password) VALUES('admin', '{$hash}')";
+Database::connect()->exec($insertData);
+print ("Inserted data into table $tableName. " . PHP_EOL);
+
+/**
+ * Create table vote
+ */
+$tableName = 'vote';
+$dropTable = "DROP TABLE IF EXISTS {$tableName}";
+Database::connect()->exec($dropTable);
+
+$createTable = "CREATE TABLE $tableName(
+  id INT( 11 ) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  event_id INT( 11 ) UNSIGNED,
+  user_id INT( 11 ) UNSIGNED,
+  winning_id INT( 11 ) UNSIGNED,
+  createdAt DATETIME NOT NULL
+  );";
+Database::connect()->exec($createTable);
+print ("Created table $tableName. " . PHP_EOL);
