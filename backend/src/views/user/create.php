6045817101b208499429c083e7802405b86b041b
Hazrat Ali <ali@triggybet.com>
2018-11-04 22:07:09 +0100

    Added css, html design

--- backend/src/views/user/create.php
+++ backend/src/views/user/create.php
@@ -10,15 +10,8 @@ include_once "{$dirPath}/layout/navbar.php";
 ?>
 <div class="container">
 
-    <div class="row">
-        <div class="col-md-12">
-            <h2>User Signup</h2>
-            <br />
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6 pb-5">
            <?php
             if($model->hasErrors()) {
                 $errors = $model->getErrors();
@@ -28,27 +21,33 @@ include_once "{$dirPath}/layout/navbar.php";
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6 pb-5">
             <form class="form-group" action="/user/signup" method="post">
-                <div class="form-group">
-                    <label class="form-control-label" for="title">Username</label>
-                    <input type="text" name="username" placeholder="Type username" class="form-control is-valid" id="username" value="<?=$model->username?>">
+                <div class="card">
+                    <div class="card-header p-4">
+                        Sign up
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label class="form-control-label" for="title">Username</label>
+                            <input type="text" name="username" placeholder="Type username" class="form-control is-valid" id="username" value="<?=$model->username?>">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label" for="description">Password</label>
+                            <input type="password" name="password" placeholder="Type password" class="form-control is-valid" id="password" value="<?=$model->password?>">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label" for="description">Verify Password</label>
+                            <input type="password" name="verifyPassword" placeholder="Verify password" class="form-control is-valid" id="verifyPassword" value="<?=$model->verifyPassword?>">
+                        </div>
+                        <br />
+                        <button type="submit" class="btn btn-outline-secondary">Sign up</button>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label class="form-control-label" for="description">Password</label>
-                    <input type="password" name="password" placeholder="Type password" class="form-control is-valid" id="password" value="<?=$model->password?>">
-                </div>
-                <div class="form-group">
-                    <label class="form-control-label" for="description">Verify Password</label>
-                    <input type="password" name="verifyPassword" placeholder="Verify password" class="form-control is-valid" id="verifyPassword" value="<?=$model->verifyPassword?>">
-                </div>
-                <br />
-                <button type="submit" class="btn btn-outline-secondary">Sign up</button>
             </form>
         </div>
     </div>
-
 
 </div>
 
--- backend/src/views/user/login.php
+++ backend/src/views/user/login.php
@@ -11,8 +11,8 @@ include_once "{$dirPath}/layout/navbar.php";
 
 <div class="container">
 
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6 pb-5">
             <?php
             if($model->hasErrors()) {
                 $errors = $model->getErrors();
@@ -22,41 +22,27 @@ include_once "{$dirPath}/layout/navbar.php";
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6 pb-5">
             <form class="form-vertical" id="login-form" action="/user/login" method="post">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
+                    <div class="card">
+                        <div class="card-header p-4">Login</div>
+                        <div class="card-body">
                             <fieldset>
+
                                 <div class="form-group">
-                                    <h2>Log In</h2>
-                                    <hr>
+                                    <label>Username</label>
+                                    <input class="form-control" placeholder="Username" name="username" id="username" type="text" value="<?=$model->username?>">
                                 </div>
                                 <div class="form-group">
-                                    <div class="col-md-3">
-                                        <label>Username</label>
-                                    </div>
-                                    <div class="col-md-9">
-                                        <div class="control-group flat-group">
-                                            <input class="form-control" placeholder="Username" name="username" id="username" type="text" value="<?=$model->username?>">
-                                        </div>
-                                    </div>
+                                    <label>Password</label>
+                                    <input class="form-control" placeholder="Password" name="password" id="password" type="password" value="<?=$model->password?>">
                                 </div>
                                 <div class="form-group">
-                                    <div class="col-md-3">
-                                        <label>Password</label>
-                                    </div>
-                                    <div class="col-md-9">
-                                        <div class="control-group flat-group">
-                                            <input class="form-control" placeholder="Password" name="password" id="password" type="password" value="<?=$model->password?>">
-                                        </div>
-                                    </div>
+                                    <br />
                                 </div>
                                 <div class="form-group">
-                                    <div class="col-md-3" style="text-align: right;"></div>
-                                    <div class="col-md-9">
                                         <input class="btn btn-success" type="submit" value="Login">
-                                    </div>
                                 </div>
                             </fieldset>
                         </div>
